3cd8d8c0496c3b2433a66a54aef8b5585d0ed1e1
Si Mohamed Rayene <raykib2000@live.fr>
2021-12-10 15:41:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
+import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import { Route, Switch } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return;
+		<Provider store={store}>
+			<Router>
+				<Switch>
+					<Route exact path="/" component={} />
+				</Switch>
+			</Router>
+		</Provider>;
+	}
 }
 
 export default App;
