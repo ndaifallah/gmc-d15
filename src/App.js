e6072b69274377bfffe03f9cd246910b865389cb
Si Mohamed Rayene <raykib2000@live.fr>
2021-12-11 10:01:54 +0100

    merged with imene

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import "antd/dist/antd.css";
 import { Provider } from "react-redux";
 import { Route, Switch } from "react-router";
 import { BrowserRouter as Router } from "react-router-dom";
-<<<<<<< HEAD
 import store from "./redux/store";
 import Home from "./containers/home";
+import LoginContainer from "./containers/logincontainer";
+import Signup from "./components/signUp";
 
 class App extends Component {
 	render() {
@@ -14,29 +15,14 @@ class App extends Component {
 			<Provider store={store}>
 				<Router>
 					<Switch>
-						<Route exact path="/" component={Home} />
+						<Route exact path="/home" component={Home} />
+						<Route exact path="/login" component={LoginContainer} />
+						<Route exact path="/signup" component={Signup} />
 					</Switch>
 				</Router>
 			</Provider>
 		);
 	}
-=======
-import LoginContainer from "./containers/logincontainer";
-import store from "./redux/store";
-
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Router>
-          <Switch>
-            <Route exact path="/login" component={LoginContainer} />
-          </Switch>
-        </Router>
-      </Provider>
-    );
-  }
->>>>>>> origin/imene
 }
 
 export default App;
